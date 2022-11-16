31a0d9c9bc1e18913e74e25dfc7462a651253589
elle-emm <lizamendez@mail.weber.edu>
2022-11-15 19:13:27 -0700

    added unit conditional

--- functions.py
+++ functions.py
@@ -13,6 +13,15 @@ user_unit = input("What unit is your number? ")
 #to convert millimeters to inches --> mm / 25.4
 
 #user gives unit in inches
-conv_number = user_number * 25.4
+#conv_number = user_number * 25.4
+conv_number = user_number / 25.4
+if (user_unit == 'in'):
+    #perform in to mm
+    conv_number = user_number * 25.4
+
+elif (user_unit == 'mm'):
+    #perform in to mm
+    conv_number = user_number / 25.4
+
 print(conv_number)
 print(user_unit)
