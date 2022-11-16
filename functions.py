fb0a57367c3970f2a5b67eef0105b06d76f5a83d
elle-emm <lizamendez@mail.weber.edu>
2022-11-15 18:58:21 -0700

    get user number

---
+++ functions.py
@@ -0,0 +1,10 @@
+#Conversion Calculator
+#By: Liza Méndez
+
+#user input regarding the length to convert
+#get the unit from the user
+#convert the length to the correct unit
+#output the answer to the user
+
+user_number = input("What number would you like to convert? ")
+print(user_number)
