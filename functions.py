a4a8b0ed8c1634403598099c87ca94f9889b2ae8
elle-emm <lizamendez@mail.weber.edu>
2022-11-15 19:17:09 -0700

    error checking for unit

--- functions.py
+++ functions.py
@@ -16,12 +16,16 @@ user_unit = input("What unit is your number? ")
 #conv_number = user_number * 25.4
 conv_number = user_number / 25.4
 if (user_unit == 'in'):
-    #perform in to mm
+    #perform mm to in
     conv_number = user_number * 25.4
+    conv_unit == 'mm'
 
 elif (user_unit == 'mm'):
     #perform in to mm
     conv_number = user_number / 25.4
+    conv_unit = 'in'
+else:
+    print('That is not a valid unit')
 
 print(conv_number)
 print(user_unit)
